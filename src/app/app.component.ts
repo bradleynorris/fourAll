38d508c69188f03b12a1b03dff64c8ff8c5feb4b
Bradley Norris <risebaxelrod@gmail.com>
2019-03-25 20:30:38 -0700

    content added, score buttons working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  
+  timerStart: number = 2000;
+  scoreL: number = 0;
+  scoreR: number = 0;
+
+  addOneL() {
+    return this.scoreL = this.scoreL + 1;
+  }
+
+  addOneR() {
+    return this.scoreR = this.scoreR + 1;
+  }
+
+  minusOneL() {
+    return this.scoreL = this.scoreL - 1;
+  }
+
+  minusOneR() {
+    return this.scoreR = this.scoreR - 1;
+  }
+
+  countDown() {
+    return this.timerStart = this.timerStart - 1;
+  }
+
+  reset() {
+    return this.timerStart = 2000;
+  }
+
+  ngOnInit () {
+    
+  }
 }
