95407125ea444a6ad7f7aa25115d4d6ac9349083
Bradley Norris <risebaxelrod@gmail.com>
2019-04-01 20:32:57 -0700

    team info functionality done.  Last thing is to get the timer buttons working correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  
-  timerStart: number = 2000;
+
+  @ViewChild('counter', {read:TimerComponent})
+  private counter: TimerComponent;
+
   scoreL: number = 0;
   scoreR: number = 0;
+
+  colors = [
+    {id: 'danger', name: "Red"},
+    {id: 'primary', name: "Blue"},
+    {id: 'success', name: "Green"},
+    {id: 'warning', name: "Yellow"},
+    {id: 'dark', name: "Black"},
+    {id: 'secondary', name: 'Grey'},
+    {id: 'info', name: 'Teal'},
+    {id: 'light', name: 'White'}
+  ];
+
+  submitTeam(form) {
+    console.log(form.value);
+  }
 
   addOneL() {
     return this.scoreL = this.scoreL + 1;
@@ -27,15 +46,17 @@ export class AppComponent implements OnInit{
     return this.scoreR = this.scoreR - 1;
   }
 
-  countDown() {
-    return this.timerStart = this.timerStart - 1;
-  }
-
-  reset() {
-    return this.timerStart = 2000;
+  refresh() {
+    window.location.reload();
   }
 
   ngOnInit () {
-    
+    this.counter.startAt = 1200;
+    this.counter.start();
+    this.counter.counterState.subscribe(state=>{
+      if (state === 'COMPLETE') {
+        alert('TIME IS UP');
+      }
+    })
   }
 }
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, Input, Output, ChangeDetectionStrategy, ChangeDetectorRef, EventEmitter } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TimerComponent implements OnInit {
+
+  @Input()
+  startAt = 0;
+
+  @Output()
+  counterState = new EventEmitter<string>();
+
+  currentValue = '';
+  currentSubscription: Subscription;
+
+  constructor(private changeDetector : ChangeDetectorRef) { }
+
+  ngOnInit() {
+  }
+
+  public start() {
+    this.currentValue = this.formatValue(this.startAt);
+    this.changeDetector.detectChanges();
+
+    const t: Observable<number> = interval(1000);
+    this.currentSubscription = t.pipe(take(this.startAt)).map(v => this.startAt - (v + 1)).subscribe(v => {
+      this.currentValue = this.formatValue(v);
+      this.changeDetector.detectChanges();
+    }, err=>{
+      this.counterState.error(err);
+    }, ()=>{
+      this.currentValue = '00:00';
+      this.counterState.emit('COMPLETE');
+      this.changeDetector.detectChanges();
+    });
+  }
+
+  public stop() {
+    this.currentSubscription.unsubscribe();
+    this.counterState.emit('ABORTED');
+  }
+
+
+
+  private formatValue(v) {
+    const minutes = Math.floor(v/60);
+    const formattedMinutes = (minutes > 9? minutes : '0' + minutes);
+
+    const seconds = v % 60;
+    const formattedSeconds = (seconds > 9? seconds : '0' + seconds);
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+
+  startTimer() {
+
+  }
+
+  pauseTimer() {
+
+  }
+
+  resetTimer() {
+    
+  }
+
+}
